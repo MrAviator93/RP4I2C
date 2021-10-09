e42c7ba1d5a2876414ab9ad0a8ae5b7552ad9af7
MrAviator93 <kalninsarturs@hotmail.com>
2021-10-09 23:02:20 +0100

    Applied clang format

--- Code/Source/I2C/main.cpp
+++ Code/Source/I2C/main.cpp
@@ -12,11 +12,11 @@ int main( int, char** )
 {
 	std::cout << "Welcome!" << std::endl;
 
-	I2C::CI2CBusController bc {"/dev/i2c-1"};
-	I2C::CLM75Controller lc {bc, 0x48};
+	I2C::CI2CBusController bc { "/dev/i2c-1" };
+	I2C::CLM75Controller lc { bc, 0x48 };
 
 	// We will use MCP here to control LED's, configure ports A & B as output
-	I2C::CMCP23017Controller mcp {bc, 0x00, 0x00, 0x20};
+	I2C::CMCP23017Controller mcp { bc, 0x00, 0x00, 0x20 };
 
 	mcp.setOnPortA( I2C::CMCP23017Controller::Pins::PIN_8,
 					I2C::CMCP23017Controller::Pins::PIN_7,
@@ -24,7 +24,7 @@ int main( int, char** )
 
 	mcp.setOffPortA( I2C::CMCP23017Controller::Pins::PIN_8 );
 
-	I2C::CBMP180Controller bmp {bc, I2C::CBMP180Controller::SamplingAccuracy::ULTRA_HIGH_RESOLUTION };
+	I2C::CBMP180Controller bmp { bc, I2C::CBMP180Controller::SamplingAccuracy::ULTRA_HIGH_RESOLUTION };
 
 	std::cerr << "True temp: " << bmp.getTrueTemperatureC() << " C" << std::endl;
 	std::cerr << "True pressure: " << bmp.getTruePressurePa() << " Pa" << std::endl;
